8972d8d4214cd53be089d8da63a0363f1f61e1aa
unknown <unknown@example.com>
2020-09-19 16:58:59 +0800

    Initial Commit

---
+++ pages/card/card.js
@@ -0,0 +1,35 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    card: {},
+    id: '',
+    qr_code: {}
+  },
+  onLoad: function (option) {
+    this.setData({
+      id: option.id,
+      qrcodeUrl: app.globalData.serverURL + "/getcode?id=" + option.id
+    });
+    wx.request({
+      url: app.globalData.serverURL + '/check',
+      data: {
+        id: this.data.id
+      },
+      header: { 'content-type': 'application/json' },
+      success: res => {
+        this.setData({
+          card: res.data
+        });
+      }
+    });
+  },
+  onShareAppMessage: function () {
+    return {
+      title: 'Share this business card',
+      path: 'pages/card/card?id=' + this.data.id
+    };
+  }
+})
